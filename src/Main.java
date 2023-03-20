45bdbc6df3d04b0e9122b8c9cddfe988c02ab8d6
Zhan Teyvans <teyvans@gmail.com>
2023-03-21 00:54:49 +0300

    Inicial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13_676;
+        int oneMile = 20;
+        int bonus = amount / oneMile;
+        System.out.println("Начислено миль за приобретенный авиабилет: " + bonus);
+    }
+}
